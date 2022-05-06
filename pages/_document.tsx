32a57a9ad5f3c59114cb2b51b7770d30599911c0
gitlherme <dev@guilhermevieira.dev>
2022-05-06 17:27:43 -0300

    feat: added chakra ui

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Head, Html, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>Wondertech 2022 | 13 de maio de 2022</title>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
