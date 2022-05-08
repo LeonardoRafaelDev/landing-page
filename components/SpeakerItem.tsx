d0cb5f69c6abece64e770fd1bd3ae82124644e02
thel3onardo <leonardo.contato03@gmail.com>
2022-05-07 22:14:06 -0300

    feat: finish speakers section

--- components/SpeakerItem.tsx
+++ components/SpeakerItem.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading, Icon, IconButton, Text } from "@chakra-ui/react"
+import { Flex, Heading, IconButton, Text, Image } from "@chakra-ui/react"
 import { BsTwitter, BsInstagram, BsLinkedin } from 'react-icons/bs'
 
 interface SpeakerItem {
@@ -10,19 +10,26 @@ interface SpeakerItem {
 
 export function SpeakerItem( { profilePic, name, role, description } : SpeakerItem) {
     const socialList = [
-        { name: BsTwitter, href: "" }
-        { name: BsInstagram, href: "" }
+        { name: BsTwitter, href: "" },
+        { name: BsInstagram, href: "" },
         { name: BsLinkedin, href: "" }
     ]
 
     return (
-        <Flex flexDir="column" paddingY="32px" paddingX="16px">
-            <Heading as="h2" size="2xl" marginTop="32px">{ name }</Heading>
-            <Heading as="h5" size="md" color="brand.green" marginTop="32px">{ role }</Heading>
-            <Text color="brand.gray" marginY="32px">{ description }</Text>
-            <Flex w="100%" justify="center" align="center">
+        <Flex bg="brand.black" maxW="322px" flexDir="column" paddingY="32px" paddingX="16px" marginLeft={{ base: '12px', md: '50px' }} marginBottom={{ base: '110px', md: '0' }} align="center">
+            <Image 
+                w={{ base: '50px', md: '100px'}}
+                h={{ base: '50px', md: '100px'}} 
+                borderRadius="50%" 
+                src='participantes/roberson.jpg' 
+                alt="teste"
+            ></Image>
+            <Heading textAlign="center" as="h2" fontSize="2xl" marginTop="32px" color="brand.white">{ name }</Heading>
+            <Heading textAlign="center" as="h5" fontSize="md" color="brand.green" marginTop="32px">{ role }</Heading>
+            <Text color="brand.gray" marginY="32px" textAlign='center'>{ description }</Text>
+            <Flex w="70%" justify="space-between" align="center">
                 {socialList.map((social) => {
-                    return <IconButton as={social.name} href={social.href} aria-label={"test"} />
+                    return <IconButton color="brand.white" bg="none" as={social.name} href={social.href} aria-label={"test"} key={social.name} boxSize="32px" background='none' />
                 })}
             </Flex>
         </Flex>
