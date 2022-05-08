51075ddc18d91dd9a6078e82170a7467003d5721
gitlherme <dev@guilhermevieira.dev>
2022-05-08 14:02:42 -0300

    change domain in metatags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,12 +12,12 @@ export default function Document() {
         <meta name="title" content="WonderTech - Encontro de Comunidades" />
         <meta name="description" content="O evento é sem fins lucrativos, todo montante doado será utilizado para a alimentação dos participantes e brindes e qualquer quantia que sobrar será revertido em doação para o núcleo de Tecnologia do MTST. Qualquer quantia é bem vinda, tanto de pessoa física, quanto empresas." />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://deploy-preview-13--wondertech2022.netlify.app/" />
+        <meta property="og:url" content="https://wondertech.dev" />
         <meta property="og:title" content="WonderTech - Encontro de Comunidades" />
         <meta property="og:description" content="O evento é sem fins lucrativos, todo montante doado será utilizado para a alimentação dos participantes e brindes e qualquer quantia que sobrar será revertido em doação para o núcleo de Tecnologia do MTST. Qualquer quantia é bem vinda, tanto de pessoa física, quanto empresas." />
         <meta property="og:image" content="https://i.imgur.com/vzhtLee.png" />
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://deploy-preview-13--wondertech2022.netlify.app/" />
+        <meta property="twitter:url" content="https://wondertech.dev" />
         <meta property="twitter:title" content="WonderTech - Encontro de Comunidades" />
         <meta property="twitter:description" content="O evento é sem fins lucrativos, todo montante doado será utilizado para a alimentação dos participantes e brindes e qualquer quantia que sobrar será revertido em doação para o núcleo de Tecnologia do MTST. Qualquer quantia é bem vinda, tanto de pessoa física, quanto empresas." />
         <meta property="twitter:image" content="https://i.imgur.com/vzhtLee.png" />
