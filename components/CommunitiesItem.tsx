0e35ab9c21237761604a0bec873de0450336e8f8
thel3onardo <leonardo.contato03@gmail.com>
2022-05-08 20:27:17 -0300

    feat: implement modal to communities and sponsors section; fix: fixes console log errors

--- components/CommunitiesItem.tsx
+++ components/CommunitiesItem.tsx
@@ -1,5 +1,6 @@
-import { Flex, Heading, Icon, Text, Image, Divider, Link } from '@chakra-ui/react';
-import { BsTwitter, BsInstagram, BsDiscord, BsGithub } from 'react-icons/bs'
+import { Flex, Heading, Icon, Text, Image, Divider, Link, useDisclosure } from '@chakra-ui/react';
+import { BsTwitter, BsInstagram, BsDiscord, BsGithub } from 'react-icons/bs';
+import { ModalComponent } from './Modal';
 
 interface Socials {
     instagram: string
@@ -17,46 +18,50 @@ interface CommunityDetails {
 }
 
 export function CommunitiesItem ({ name, description, imgUrl, imgAlt, socials } : CommunityDetails) {
+    const { isOpen, onOpen, onClose } = useDisclosure();
     return (
-        <Flex bg="brand.black" maxW="544px" px="20px" py="32px" marginBottom={{ base: '80px', md: '0' }} marginLeft={{ md: '48px' }} minHeight="300px">
-            <Flex justify="center" align="center" maxWidth={{ base: '30px', md: '50px'}} maxH={{ base: '30px', md: '50px'}} borderRadius="50%" overflow="hidden" bg="brand.darkGray" marginRight={{ base: '16px', md: '32px'}}>
-                <Image maxW="70%" src={imgUrl} alt={imgAlt} borderRadius="50%"></Image>
-            </Flex>
-            <Flex
-                flexDir='column'
-                justify='center'
-            >
-                <Heading 
-                    as="h4" 
-                    fontWeight="bold" 
-                    fontSize="2xl" 
-                    color="brand.white"
-                    paddingBottom="1rem"
+        <>
+            <Flex bg="brand.black" maxW="544px" px="20px" py="32px" marginBottom={{ base: '80px', md: '0' }} marginLeft={{ md: '48px' }} cursor="pointer" minHeight="320px" onClick={onOpen}>
+                <Flex justify="center" align="center" maxWidth={{ base: '30px', md: '50px'}} maxH={{ base: '30px', md: '50px'}} borderRadius="50%" overflow="hidden" bg="brand.darkGray" marginRight={{ base: '16px', md: '32px'}}>
+                    <Image maxW="70%" src={imgUrl} alt={imgAlt} borderRadius="50%"></Image>
+                </Flex>
+                <Flex
+                    flexDir='column'
+                    justify='center'
                 >
-                    { name }
-                </Heading>
-                <Text
-                    color="brand.gray"
-                    noOfLines={4}
-                >
-                    { description }
-                </Text>
-                <Divider borderColor="brand.gray" my="6" />
-                <Flex paddingTop="28px">
-                    <Link href={socials.twitter} isExternal>
-                        <Icon color="brand.white" as={BsTwitter} boxSize="2rem" marginRight="38px" cursor="pointer" transition="color .5s ease" _hover={{ color: 'brand.green' }} />
-                    </Link>
-                    <Link href={socials.instagram} isExternal>
-                        <Icon color="brand.white" as={BsInstagram} boxSize="2rem" marginRight="38px" cursor="pointer" transition="color .5s ease" _hover={{ color: 'brand.green' }} />
-                    </Link>
-                    <Link href={socials.discord} isExternal>
-                        <Icon color="brand.white" as={BsDiscord} boxSize="2rem" marginRight="38px" cursor="pointer" transition="color .5s ease" _hover={{ color: 'brand.green' }} />
-                    </Link>
-                    <Link href={socials.github} isExternal>
-                        <Icon color="brand.white" as={BsGithub} boxSize="2rem" marginRight="38px" cursor="pointer" transition="color .5s ease" _hover={{ color: 'brand.green' }} />
-                    </Link>
+                    <Heading 
+                        as="h4" 
+                        fontWeight="bold" 
+                        fontSize="2xl" 
+                        color="brand.white"
+                        paddingBottom="1rem"
+                    >
+                        { name }
+                    </Heading>
+                    <Text
+                        color="brand.gray"
+                        noOfLines={4}
+                    >
+                        { description }
+                    </Text>
+                    <Divider borderColor="brand.gray" my="6" />
+                    <Flex paddingTop="28px">
+                        <Link href={socials.twitter} isExternal>
+                            <Icon color="brand.white" as={BsTwitter} boxSize="2rem" marginRight="38px" cursor="pointer" transition="color .5s ease" _hover={{ color: 'brand.green' }} />
+                        </Link>
+                        <Link href={socials.instagram} isExternal>
+                            <Icon color="brand.white" as={BsInstagram} boxSize="2rem" marginRight="38px" cursor="pointer" transition="color .5s ease" _hover={{ color: 'brand.green' }} />
+                        </Link>
+                        <Link href={socials.discord} isExternal>
+                            <Icon color="brand.white" as={BsDiscord} boxSize="2rem" marginRight="38px" cursor="pointer" transition="color .5s ease" _hover={{ color: 'brand.green' }} />
+                        </Link>
+                        <Link href={socials.github} isExternal>
+                            <Icon color="brand.white" as={BsGithub} boxSize="2rem" marginRight="38px" cursor="pointer" transition="color .5s ease" _hover={{ color: 'brand.green' }} />
+                        </Link>
+                    </Flex>
                 </Flex>
             </Flex>
-        </Flex>
+            <ModalComponent isOpen={isOpen} onOpen={onOpen} onClose={onClose} title={name} content={description} profilePic={imgUrl} />
+        </>
     )
 }
--- components/Sponsor.tsx
+++ components/Sponsor.tsx
@@ -8,7 +8,7 @@ interface SponsorProps {
 export function Sponsor({ logo, href }: SponsorProps) {
   return (
     <Link href={href} target="_blank">
-      <Image src={logo} w={["80px", "100px"]} h={["80px", "100px"]} borderRadius="50%" cursor="pointer"/>
+      <Image src={logo} w={["80px", "100px"]} h={["80px", "100px"]} borderRadius="50%" cursor="pointer" alt="Image alt"/>
     </Link>
   )
 }
