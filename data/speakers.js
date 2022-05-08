d77b96f0e9c78a9861a4bec3602b6a00c435414f
= <=>
2022-05-07 23:44:40 -0300

    feat(data): add speakers

---
+++ data/speakers.js
@@ -0,0 +1,127 @@
+export const speakers = [
+  {
+    id: 1,
+    name: 'Anne',
+    age: 22,
+    city: 'São Paulo',
+    state: 'SP',
+    role: 'Front-end Developer',
+    socials: {
+      twitter: 'https://twitter.com/annerland',
+      instagram: 'https://instagram.com/annerland',
+      linkedin: 'https://www.linkedin.com/in/anna-campelo-559606152'
+    },
+    bio: 'Artista, Desenvolvedora e membro da He4rt Developers'
+  },
+  {
+    id: 2,
+    name: 'Isabella',
+    age: 23,
+    city: 'Curitiba',
+    state: 'PR',
+    role: 'Product Manager',
+    socials: {
+      twitter: 'http://twitter.com/isahermanx',
+      instagram: 'http://instagram.com/isabellaherman',
+      linkedin: 'https://www.linkedin.com/in/anna-campelo-559606152'
+    },
+    bio: 'Gamedev Evangelist and MAGE founder'
+  },
+  {
+    id: 3,
+    name: 'Letícia',
+    role: 'Full-Stack Software Engineer',
+    age: 28,
+    city: 'Palhoça',
+    state: 'SC',
+    socials: {
+      twitter: 'https://twitter.com/EngineerRabbit',
+      instagram: 'http://instagram.com/isabellaherman',
+      linkedin: 'https://www.linkedin.com/in/anna-campelo-559606152'
+    },
+  },
+  {
+    id: 4,
+    name: 'João Gabriel',
+    age: 37,
+    role: 'Head of Technology',
+    bio: 'Dev e Head de tecnologia com mais de 14 anos de experiência na área tech.',
+    socials: {
+      twitter: 'https://mobile.twitter.com/joao_gabriel_08',
+      instagram: 'https://www.instagram.com/o_matuto_programador/',
+      linkedin: 'https://www.linkedin.com/in/joao-gabriel-matuto/'
+    }
+  },
+  {
+    id: 5,
+    name: Kamila,
+    age: 24,
+    city: 'São Paulo',
+    state: 'SP',
+    role: 'Teach Lead',
+    bio: 'Trabalho na área de tecnologia desde 2017, Desenvolvedora backend com foco em Microsserviços em Java e Spring, bacharel em ciência da computação, cursando MBA em arquitetura de soluções, sou Microsoft MVP na categoria de Developer Technologies, co-organizadora de comunidades de tecnologia e sou co-autora do livro Jornada Java (escrito em conjunto com outras 30 pessoas). Produzo conteúdo no instagram e youtube Kamila_code e no experts club da rocketseat',
+    socials: {
+      twitter: 'https://twitter.com/kamilah_santos',
+      instagram: 'https://www.instagram.com/kamila_code/',
+      linkedin: 'https://www.linkedin.com/in/kamila-santos-oliveira/'
+    }
+  },
+  {
+    id: 6,
+    name: 'Fernanda',
+    age: 37,
+    city: 'Ferraz de Vasconcelos',
+    state: 'São Paulo',
+    role: 'Live coder',
+    bio: 'Estuda Análise e Desenvolvimento de Sistemas (FATEC Ferraz de Vasconcelos). Integra as comunidades Wakanda Streamers e Feministech, além de ter feito lives de estudo de programação na twitch, onde compartilhou seus conhecimento e desafios em estar se graduando após os 30 anos. Compartilha em suas redes sociais questões sobre diversidade para situações que estão além do óbvio.',
+    socials: {
+      twitter: 'https://twitter.com/leitoraincomum',
+      instagram: 'https://instagram.com/leitoraincomum',
+      linkedin: 'https://linkedin.com/in/leitoraincomum',
+    }
+  },
+  {
+    id: 7,
+    name: 'Roberson',
+    age: 42,
+    city: 'São Paulo',
+    state: 'São Paulo',
+    role: 'Software Developer',
+    bio: 'Pai de meninas | Desenvolvedor de software | co-organizador na perifaCode | Cicloativista',
+    socials: {
+      instagram: 'https://instagram.com/_sou_o_roberson',
+      twitter: 'https://twitter.com/biosbug',
+      linkedin: 'https://www.linkedin.com/in/roberson-miguel'
+    }
+  },
+
+  {
+    id: 8,
+    name: Vanessa,
+    age: 30,
+    city: 'Orlando',
+    state: 'FL',
+    role: 'Community Manager',
+    bio: 'Administradora de Banco de Dados, desenvolvedora SQL e Python e eterna aprendiz de front end. Atualmente tocando as comunidades da Stack Overflow como Community Manager, criadora de conteúdo em tecnologia, apaixonada por flamingos e pela Wonder Woman.',
+    socials: {
+      twitter: 'https://twitter.com/WonderWanny',
+      instagram: 'https://www.instagram.com/wonderwanny/',
+      linkedin: 'https://www.linkedin.com/in/kamila-santos-oliveira/'
+    }
+  },
+  {
+    id: 9,
+    name: 'Glauber',
+    age: 32,
+    state: 'São Paulo',
+    city: 'SP',
+    role: 'Project Manager',
+    bio: 'Nascido, criado e morando o Grajaú, extremo da sul. Sou casado, tenho uma filha de 4 anos e tento deixar um mundo melhor pra ela. Trabalho como Product Manager no Itaú e como organizador na perifaCode. Somente a Educação é capaz de mudar vidas.',
+    socials: {
+      instagram: 'https://instagram.com/glauberacastro',
+      twitter: 'https://twitter.com/glauberacastro',
+      linkedin: 'https://www.linkedin.com/in/glaubercastro'
+    }
+  }
+
+]
